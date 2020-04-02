a994e5259040f5b582152b3310216ca79d879ffd
Michael Petersen <mig.mic@gmail.com>
2020-04-02 13:21:21 +0200

    ss

---
+++ src/main/java/DepartmentDAO.java
@@ -0,0 +1,14 @@
+import java.sql.Connection;
+
+public class DepartmentDAO {
+
+    private Connection conn;
+
+        public DepartmentDAO(Connection conn) {
+            this.conn=conn;
+
+        }
+
+
+
+}
---
+++ src/main/java/DepartmentDTO.java
@@ -0,0 +1,16 @@
+public class DepartmentDTO {
+    private int deptno;
+    private String dname;
+    private String loc;
+
+
+    public DepartmentDTO(){}
+
+    public DepartmentDTO(int deptno,String dname, String loc)
+    {
+
+
+    }
+
+
+}
