cf6de73c45bf24fc6db0f14ed33f9f800a5a796c
Michael Petersen <mig.mic@gmail.com>
2020-04-02 14:11:29 +0200

    New test

--- src/main/java/DepartmentDAO.java
+++ src/main/java/DepartmentDAO.java
@@ -1,4 +1,7 @@
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class DepartmentDAO {
 
@@ -9,6 +12,25 @@ public class DepartmentDAO {
 
         }
 
+        public DepartmentDTO getSingleDepartmentById(int id) {
+           DepartmentDTO objectToReturn = new DepartmentDTO();
 
+
+           try {
+               PreparedStatement getSingleStatement = conn.prepareStatement("select * from dept where deptno=?");
+               getSingleStatement.setInt(1,id);
+               ResultSet singleDepartment = getSingleStatement.executeQuery();
+               while(singleDepartment.next()) {
+                   objectToReturn.setDeptno(singleDepartment.getInt("deptno"));
+                   objectToReturn.setDname(singleDepartment.getString("dname"));
+                   objectToReturn.setLoc(singleDepartment.getString("loc"));
+               }
+           }
+                catch (SQLException e) {
+                    e.printStackTrace();
+
+                }
+                return objectToReturn;
+        }
 
 }
--- src/main/java/EmployeeDTO.java
+++ src/main/java/EmployeeDTO.java
@@ -1,2 +1,92 @@
+import java.util.Date;
+
 public class EmployeeDTO {
+
+    private int empno;
+    private String ename;
+    private String job;
+    private int mgr;
+    private Date hiredate;
+    private int sale;
+    private int comm;
+    private int deptno;
+
+
+    public  EmployeeDTO() {}
+
+
+    public EmployeeDTO(int empno, String ename, String job, int mgr, Date hiredate, int sale, int comm, int deptno) {
+        this.empno = empno;
+        this.ename = ename;
+        this.job = job;
+        this.mgr = mgr;
+        this.hiredate = hiredate;
+        this.sale = sale;
+        this.comm = comm;
+        this.deptno = deptno;
+    }
+
+    public int getEmpno() {
+        return empno;
+    }
+
+    public void setEmpno(int empno) {
+        this.empno = empno;
+    }
+
+    public String getEname() {
+        return ename;
+    }
+
+    public void setEname(String ename) {
+        this.ename = ename;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public int getMgr() {
+        return mgr;
+    }
+
+    public void setMgr(int mgr) {
+        this.mgr = mgr;
+    }
+
+    public Date getHiredate() {
+        return hiredate;
+    }
+
+    public void setHiredate(Date hiredate) {
+        this.hiredate = hiredate;
+    }
+
+    public int getSale() {
+        return sale;
+    }
+
+    public void setSale(int sale) {
+        this.sale = sale;
+    }
+
+    public int getComm() {
+        return comm;
+    }
+
+    public void setComm(int comm) {
+        this.comm = comm;
+    }
+
+    public int getDeptno() {
+        return deptno;
+    }
+
+    public void setDeptno(int deptno) {
+        this.deptno = deptno;
+    }
 }
